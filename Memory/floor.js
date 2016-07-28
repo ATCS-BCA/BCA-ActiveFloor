053f1735dbe00074c0d12768d53d34f3b509e4ac
coding-in-c <cmsmforever@yahoo.com>
2016-07-28 08:55:27 -0400

    restart

--- Memory/floor.js
+++ Memory/floor.js
@@ -318,7 +318,7 @@ function endScreen() {
     }
 
     // restart button
-    context2D.rect(canvas.width / 2 - (context2D.measureText('Restart').width / 2), canvas.height / 2 + 30, context2D.measureText("Restart").width, 20);
+    context2D.rect(canvas.width / 2 - (context2D.measureText('Restart').width / 2), canvas.height / 2 + 20, context2D.measureText("Restart").width, 20);
     context2D.stroke();
     context2D.fillText("Restart", (canvas.width / 2 - (context2D.measureText('Restart').width / 2)), canvas.height / 2 + 30);
 
