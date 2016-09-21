5477fd1a5ad300cc42ff0780f21585f1aaf8d0a8
Aboudi Rai <Aboudi Rai>
2016-09-21 10:26:26 -0400

    fix canvas

--- ActiveInterface/interface.js
+++ ActiveInterface/interface.js
@@ -62,7 +62,7 @@ function checkForStart(dataArr){
                 if(i > 16 && (j > 2 && j < 22)){
                     console.log("selected");
                     menuPage = false;
-                    window.open("..\\dodgeball/dodgeball.html");
+                    window.location("..dodgeball/dodgeball.html");
                 }
             }
         }
