6684e7140fce30f7985e058cc2230045137a7c2e
Bryan Respass <bryres@bergen.org>
2016-09-21 10:36:53 -0400

    stuff

--- ActiveInterface/interface.js
+++ ActiveInterface/interface.js
@@ -62,7 +62,7 @@ function checkForStart(dataArr){
                 if(i > 16 && (j > 2 && j < 22)){
                     console.log("selected");
                     menuPage = false;
-                    window.location = "..dodgeball/dodgeball.html";
+                    window.location = "../Pong/pong.html";
                 }
             }
         }
