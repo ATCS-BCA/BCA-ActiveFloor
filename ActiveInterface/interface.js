a63d79a91ed5fc7c282695edaa7b6fad87428612
Iris Kim <neeks.val19@gmail.com>
2016-09-28 09:58:38 -0400

    pls draw the space

--- ActiveInterface/interface.js
+++ ActiveInterface/interface.js
@@ -4,11 +4,20 @@ var dataHolderArray = [];
 var charSearch = '*';
 var charDivide = ',';
 var canvas, context2D;
-var menuPage = true;
+var menuPage = false;
 var refreshTime = 17;
+var startBtn;
+
+var canvas = document.createElement('canvas');
+var canvasContext = canvas.getContext('2d');
+
 window.onload = function(){
 
     var framesPerSecond = 60;
+    canvas.width = 192;
+    canvas.height = 192;
+
+    setObjects();
 
 };
 
@@ -40,7 +49,7 @@ function refreshXML() {
             dataHolderArray.push(n);
         });
 
-        if (menuPage){
+        if (!menuPage){
             checkForStart(dataHolderArray);
         }
         drawBoard(dataHolderArray);
@@ -56,17 +65,43 @@ function drawBoard(dataArr){
 }
 
 function checkForStart(dataArr){
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+    canvasContext.fillStyle = 'black';
+
+    canvasContext.strokeStyle = 'black';
+    canvasContext.stokeRect(startBtn.bx, startBtn.by, startBtn.bw, startBtn.bh)
+
+
     for(var i = 0; i < dataArr.length; i++){
         for(var j = 0; j < dataArr[i].length;j++){
             if(dataArr[i][j] === "*"){
                 if(i > 16 && (j > 2 && j < 22)){
                     console.log("selected");
-                    menuPage = false;
-
-
+                    menuPage = true;
                     window.location = "menu.html";
                 }
             }
+        }
+    }
+}
+
+function setObjects() {
+    startBtn = {
+        x: (canvas.width*2 / 3) - (context2D.measureText('Step to Begin').width / 2),
+        y: 160,
+        w: context2D.measureText('Step to Begin').width,
+        h: 15,
+        bx: (canvas.width / 2) - (context2D.measureText('Step To Begin').width / 2) - 20,
+        by: 160 - 15,
+        bw: context2D.measureText('Step To Begin').width + 40,
+        bh: 15 + 5
+    }
+}
+
+function displayUrls(dataArr) {
+    for (var i = 0; i < dataArr.length; i++) {
+        for (var j = 0; j  < dataArr[i].length; j++) {
+              
         }
     }
 }
