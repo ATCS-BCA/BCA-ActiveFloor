626c205b72e60f3437045e6a7047c19f5b8aaa2a
victoriastaada <victoriastaada@gmail.com>
2016-10-26 13:12:14 -0400

    create ultimate tic tac toe

--- _Workspaces/vicsta/Demo/floor.js
+++ _Workspaces/vicsta/Demo/floor.js
@@ -10,7 +10,7 @@ var refreshTime = 17;       // Run the loop every 17 milliseconds
 
 function drawObj(type, xPos, yPos, size) {
     'use strict';
-    context2D.fillStyle = 'red';
+    context2D.fillStyle = 'blue';
 
     if (type === 'square') {
         context2D.fillRect((xPos + (xCenter / size)), (yPos + (yCenter / size)), size, size);
@@ -38,7 +38,7 @@ function drawCanvas(arr) {
             if (srchStr === charSearch) {
                 tempX = p * ledPerSensorX;
                 tempY = i * ledPerSensorY;
-				drawObj('square', tempX, tempY, 5);
+				drawObj('circle', tempX, tempY, 2);
             }
         }
     }
@@ -46,7 +46,7 @@ function drawCanvas(arr) {
 
 function loop() {
     'use strict';
-    $.get('http://127.0.0.1:8080/', function (data) {
+    $.get('http://168.229.106.80:8080/', function (data) {
         dataHolderArray = [];
 
         /* Assign the fields from the XML to Javascript variables. */
