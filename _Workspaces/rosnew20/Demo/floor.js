ce9edb02dc1e64ab9a34266b90550eff549cd0d4
Ross3102 <rosnew20@bergen.org>
2016-10-31 09:32:32 -0400

    Made changes to my demo

--- _Workspaces/rosnew20/Demo/floor.js
+++ _Workspaces/rosnew20/Demo/floor.js
@@ -10,7 +10,7 @@ var refreshTime = 17;       // Run the loop every 17 milliseconds
 
 function drawObj(type, xPos, yPos, size) {
     'use strict';
-    context2D.fillStyle = 'blue';
+    context2D.fillStyle = 'red';
 
     if (type === 'square') {
         context2D.fillRect((xPos + (xCenter / size)), (yPos + (yCenter / size)), size, size);
