c080a6c686247c5c81894051613c93d79281bab8
Iris Kim <iris.val19@gmail.com>
2016-11-16 09:15:33 -0500

    dead

--- ActiveInterface/menu.js
+++ ActiveInterface/menu.js
@@ -22,13 +22,60 @@ function initMenu(){
 
 }
 
-function loadDoc() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            alert(this.responseText);
-        }
-    };
-    xhttp.open("GET", "C:xampp/htdocs/Release.blast", true);
-    xhttp.send();
-}
+    // var xhttp = new XMLHttpRequest();
+    // xhttp.onreadystatechange = function() {
+    //     if (this.readyState == 4 && this.status == 200) {
+    //         alert(this.responseText);
+    //     }
+    // };
+    // xhttp.open("GET", "C:xampp/htdocs/Release.blast", true);
+    // xhttp.send();
+    // var rawFile = new XMLHttpRequest();
+    // rawFile.open("GET", "localhost/release.blast", false);
+    // rawFile.onreadystatechange = function ()
+    // {
+    //     if(rawFile.readyState === 4)
+    //     {
+    //         if(rawFile.status === 200 || rawFile.status == 0)
+    //         {
+    //             var allText = rawFile.responseText;
+    //             alert(allText);
+    //         }
+    //     }
+    // }
+    // rawFile.send(null);
+    function loadXMLDoc() {
+      var xhr = new XMLHttpRequest();
+        xhr.open("GET", "localhost/release.blast     ", true);
+        xhr.onload = function (e) {
+          if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+              console.log(xhr.responseText);
+            } else {
+              console.error(xhr.statusText);
+            }
+          }
+        };
+        xhr.onerror = function (e) {
+          console.error(xhr.statusText);
+        };
+        xhr.send(null);
+    }
+
+// function readTextFile()
+// {
+//     var rawFile = new XMLHttpRequest();
+//     rawFile.open("GET", file, false);
+//     rawFile.onreadystatechange = function ()
+//     {
+//         if(rawFile.readyState === 4)
+//         {
+//             if(rawFile.status === 200 || rawFile.status == 0)
+//             {
+//                 var allText = rawFile.responseText;
+//                 alert(allText);
+//             }
+//         }
+//     }
+//     rawFile.send(null);
+// }
