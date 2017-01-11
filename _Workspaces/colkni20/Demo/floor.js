0757fa64d00dfcf084a78802d044d7c87a6bce63
Bryan Respass <bryres@bergen.org>
2017-01-11 10:25:56 -0500

    updated url to dns name

--- _Workspaces/colkni20/Demo/floor.js
+++ _Workspaces/colkni20/Demo/floor.js
@@ -3,14 +3,20 @@
 var myInterval;
 var $item, ledsX, ledsY, sensorsX, sensorsY, ledPerSensorX, ledPerSensorY, xCenter, yCenter;
 var dataHolderArray = [];
+var screenArray = [];
 var charSearch = '*';
 var charDivide = ',';
 var canvas, context2D;
 var refreshTime = 17;       // Run the loop every 17 milliseconds
+for (a = 0; a < 24; a++){
+    for (b = 0; b < 24; b++){
+        screenArray[a][b] = false;
+    }
+}
 
 function drawObj(type, xPos, yPos, size) {
     'use strict';
-    context2D.fillStyle = 'green'
+    context2D.fillStyle = 'purple'
 
     if (type === 'square') {
         context2D.fillRect((xPos + (xCenter / size)), (yPos + (yCenter / size)), size, size);
@@ -22,23 +28,36 @@ function drawObj(type, xPos, yPos, size) {
     }
 }
 
-function drawCanvas(arr) {
+function updateScreenArray(arr) {
     'use strict';
     canvas = document.getElementById('floorCanvas');
     canvas.width = ledsX;
     canvas.height = ledsY;
     context2D = canvas.getContext('2d');
-    
+
     var i, tempRow, p, srchStr, tempX, tempY;
     for (i = 0; i < arr.length; i += 1) {
         tempRow = arr[i];
-        
+
         for (p = 0; p < tempRow.length; p += 1) {
             srchStr = tempRow.substring(p, p + 1);
             if (srchStr === charSearch) {
+                screenArray[i][p] = true;
+            }
+        }
+    }
+}
+
+function drawScreenArray() {
+
+    for (i = 0; i < screenArray.length; i += 1) {
+        tempRow = arr[i];
+
+        for (p = 0; p < tempRow.length; p += 1) {
+            if (screenArray[i][p]) {
                 tempX = p * ledPerSensorX;
                 tempY = i * ledPerSensorY;
-				drawObj('square', tempX, tempY, 5);
+                drawObj('square', tempX, tempY, 5);
             }
         }
     }
@@ -74,13 +93,16 @@ function loop() {
         });
 
         /* Redraw the screen based upon the data in the array. */
-        drawCanvas(dataHolderArray);
+        updateScreenArray(dataHolderArray);
+        drawScreenArray();
     });
 }
 
 $(document).ready(function () {
     'use strict';
-    
+
+    // Default screen array to 24x24 and set to false
+
     // Start getting floor data automatically (assuming Floor Server is running).
     startRefresh();
     
