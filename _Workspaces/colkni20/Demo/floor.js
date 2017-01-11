04140d215e7d4237585cec39fd35e579196cce2c
Bryan Respass <bryres@bergen.org>
2017-01-11 10:48:02 -0500

    trying to create array.

--- _Workspaces/colkni20/Demo/floor.js
+++ _Workspaces/colkni20/Demo/floor.js
@@ -3,14 +3,17 @@
 var myInterval;
 var $item, ledsX, ledsY, sensorsX, sensorsY, ledPerSensorX, ledPerSensorY, xCenter, yCenter;
 var dataHolderArray = [];
+var screenArray = [];
+var newarray = [];
 var charSearch = '*';
 var charDivide = ',';
 var canvas, context2D;
 var refreshTime = 17;       // Run the loop every 17 milliseconds
 
+
 function drawObj(type, xPos, yPos, size) {
     'use strict';
-    context2D.fillStyle = 'green'
+    context2D.fillStyle = 'purple'
 
     if (type === 'square') {
         context2D.fillRect((xPos + (xCenter / size)), (yPos + (yCenter / size)), size, size);
@@ -21,34 +24,45 @@ function drawObj(type, xPos, yPos, size) {
         context2D.fill();
     }
 }
-
-function drawCanvas(arr) {
+function updateScreenArray(arr) {
     'use strict';
-    canvas = document.getElementById('floorCanvas');
-    canvas.width = ledsX;
-    canvas.height = ledsY;
-    context2D = canvas.getContext('2d');
-    
-    var i, tempRow, p, srchStr, tempX, tempY;
-    for (i = 0; i < arr.length; i += 1) {
-        tempRow = arr[i];
-        
-        for (p = 0; p < tempRow.length; p += 1) {
+     canvas = document.getElementById('floorCanvas');
+     canvas.width = ledsX;
+     canvas.height = ledsY;
+     context2D = canvas.getContext('2d');
+
+     var i, tempRow, p, srchStr, tempX, tempY;
+     for (i = 0; i < arr.length; i += 1) {
+         tempRow = arr[i];
+
+         for (p = 0; p < tempRow.length; p += 1) {
             srchStr = tempRow.substring(p, p + 1);
             if (srchStr === charSearch) {
+                screenArray[i][p] = true;
+            }
+         }
+     }
+}
+
+function drawScreenArray() {
+
+    for (i = 0; i < screenArray.length; i += 1) {
+        tempRow = screenArray[i];
+
+        for (p = 0; p < tempRow.length; p += 1) {
+            if (screenArray[i][p]) {
                 tempX = p * ledPerSensorX;
                 tempY = i * ledPerSensorY;
-				drawObj('square', tempX, tempY, 5);
-            }
+                drawObj('square', tempX, tempY, 5);
+             }
         }
     }
 }
 
+
 function loop() {
     'use strict';
     $.get('http://activefloor.bca.bergen.org:8080/', function (data) {
-        dataHolderArray = [];
-
         /* Assign the fields from the XML to Javascript variables. */
         $(data).find('BLFloor').each(function () {
             $item = $(this);
@@ -69,27 +83,41 @@ function loop() {
             rowNum = $row.attr('rownum');
             rowVal = $row.attr('values');
             n = rowVal.split(charDivide).join('');
-				
+
             dataHolderArray.push(n);
         });
 
         /* Redraw the screen based upon the data in the array. */
-        drawCanvas(dataHolderArray);
+        updateScreenArray(dataHolderArray);
+        drawScreenArray();
     });
 }
 
 $(document).ready(function () {
     'use strict';
-    
+
+    // Default screen array to 24x24 and set to false
+//    for (i = 0; i < 24; i++)
+//        screenArray.push([false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]);
+    screenArray = new Array(24);
+    for (var i = 0; i < 24; i++) {
+        screenArray[i] = new Array(24);
+    }
+
+    for (var a = 0; a < 24; a++){
+        for (var b = 0; b < 24; b++){
+            screenArray[a][b] = false;
+        }
+    }
     // Start getting floor data automatically (assuming Floor Server is running).
     startRefresh();
-    
+
     sendSemaphore(function() {
         // Clear spacing and borders.
         $("body").addClass("app");
         $("div").addClass("app");
         $("#floorCanvas").addClass("app");
-        
+
     });
 });
 
