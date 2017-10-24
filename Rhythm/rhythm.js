04e5b599844f14fe49b5b9e724d992f4049d1613
Bryan Respass <bryres@bergen.org>
2017-10-24 16:11:02 -0400

    edited rhythm

--- Rhythm/rhythm.js
+++ Rhythm/rhythm.js
@@ -6,7 +6,9 @@ var canvas, context2D;
 
 function createCanvas() {
     canvas = document.getElementById('floorCanvas');
-    var context2D = canvas.getContext('2d');
+    canvas.width = 192;
+    canvas.height = 192;
+    context2D = canvas.getContext('2d');
 }
 
 function Ball() {
@@ -17,23 +19,23 @@ function Ball() {
 }
 
 Ball.prototype.drawCircle = function() {
-    context2D.fillColor = "#FF0000";
     context2D.beginPath();
     context2D.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
     context2D.closePath();
+    context2D.fillColor = "#FF0000";
     context2D.fill();
 }
 
 function addCircle() {
     balls.push(new Ball());
+    requestAnimationFrame(draw);
 }
 
 function draw() {
     for(var i = 0; i < balls.length; i++) {
         balls[i].drawCircle();
     }
-
-    requestAnimationFrame(draw);
+    
 }
 
 function start() {
