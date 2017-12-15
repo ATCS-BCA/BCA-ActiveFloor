b77a117ed0dde7410a0b2dda22d2acedaba08e60
adapap <adapap55555@gmail.com>
2017-12-15 08:18:31 -0500

    Diagnostic v2

--- HiLo/render.js
+++ HiLo/render.js
@@ -32,6 +32,7 @@ var Render = {
     Draw.rectFill(0,0,cw,ch,'black')
 },
 floorStatus: () => {
+    	console.log(Floor.tiles)
     for (let row in Floor.tiles) {
         for (let col in Floor.tiles[row]) {
             switch(Floor.tiles[row][col]) {
