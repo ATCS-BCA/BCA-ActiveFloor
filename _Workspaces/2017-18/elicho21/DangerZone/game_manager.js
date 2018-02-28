eb52dbe7797ca03d0540e22f691fe679d272db95
Elias <elicho21@bergen.org>
2018-02-28 09:23:25 -0500

    Activating lines

--- _Workspaces/2017-18/elicho21/DangerZone/game_manager.js
+++ _Workspaces/2017-18/elicho21/DangerZone/game_manager.js
@@ -1,30 +1,27 @@
 var time = 0;
+var lifespan = 4;
 var zones = [];
 
 /* Zone Object
 {
     "type" = "";
     "points" = [[x1, y1],... [xn, yn]];
+    "lastSpawned" = 0;
+    "activated" = false;
 }
- */
+*/
 
 function drawScreen() {
-    for (var i = 0; i < zones.length; i++) {
-        if (zones[i].type === "line") {
-            var x1 = zones[i].points[0][0];
-            var y1 = zones[i].points[0][1];
-            var x2 = zones[i].points[1][0];
-            var y2 = zones[i].points[1][1];
-            drawPoint(x1, y1);
-            drawPoint(x2, y2);
+    manageZones();
+    drawZones();
+}
 
-            context2D.beginPath();
-            context2D.moveTo(x1, y1);
-            context2D.lineTo(x2, y2);
-            context2D.closePath();
-            context2D.strokeStyle = "white";
-            context2D.stroke();
-        }
+function manageZones() {
+    for (var i = 0; i < zones.length; i++) {
+        if (time >= zones[i].lastSpawned + lifespan)
+            zones.splice(i, 1);
+        if (time >= zones[i].lastSpawned + lifespan - 1)
+            zones[i].activated = true;
     }
 }
 
@@ -34,10 +31,15 @@ function createLine() {
         "points": [
             [Math.floor(Math.random() * 192), Math.floor(Math.random() * 192)],
             [Math.floor(Math.random() * 192), Math.floor(Math.random() * 192)]
-            ]
+            ],
+        "lastSpawned": time,
+        "activated": false,
+        "function": function () {}
         });
 }
 
 function manageTap(posX, posY) {
+    for (var i = 0; i < zones.length; i++) {
 
+    }
 }
--- _Workspaces/2017-18/elicho21/DangerZone/render.js
+++ _Workspaces/2017-18/elicho21/DangerZone/render.js
@@ -5,3 +5,28 @@ function drawPoint (xPos, yPos) {
     context2D.closePath();
     context2D.fill();
 }
+
+function drawZones() {
+    for (var i = 0; i < zones.length; i++) {
+        if (zones[i].type === "line") {
+            drawLine(i);
+        }
+    }
+}
+
+function drawLine(id) {
+    var x1 = zones[id].points[0][0];
+    var y1 = zones[id].points[0][1];
+    var x2 = zones[id].points[1][0];
+    var y2 = zones[id].points[1][1];
+    drawPoint(x1, y1);
+    drawPoint(x2, y2);
+
+    context2D.beginPath();
+    context2D.moveTo(x1, y1);
+    context2D.lineTo(x2, y2);
+    context2D.closePath();
+    context2D.strokeStyle = "white";
+    context2D.lineWidth = (zones[id].activated) ? 16 : 2;
+    context2D.stroke();
+}
