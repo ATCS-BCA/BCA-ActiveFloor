2df099e91df5c0b90b9df1756b8f526eead78f31
Cole K <colkni20@bergen.orgg>
2019-06-11 14:30:55 -0400

    commit

--- ActivePaint/debugger.js
+++ ActivePaint/debugger.js
@@ -304,7 +304,7 @@ function updateScreenArray(arr) {
                         layerArray[currentLayer].arr[i][p].color = "none";
 
                     }
-                    if(brushcolor!=="eraser"){
+                    else if (!layerArray[currentLayer].value){
                         console.log("writing");
                         (layerArray[currentLayer].arr[i][p]).value = true;
                         (layerArray[currentLayer].arr[i][p]).color = brushcolor;
